c901cbe74c3a1af01d8becfafdb24d0a04946a1e
vicentepaula <vicente.silva@arcomix.com.br>
2024-01-22 09:20:09 -0300

    webcore_jenkis

---
+++ WebCore_teste/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace WebCore_teste.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
